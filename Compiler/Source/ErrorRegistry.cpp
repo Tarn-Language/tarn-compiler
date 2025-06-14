f6612b0c9670064ef529c2a0a3f8288e2f26afbb
Tarn Holder <contact@tarn-language.com>
2025-06-14 17:50:36 -0400

    Update `(Compiler\Source\ErrorRegistry.cpp)` -> Definition isn't needed but added anyway

---
+++ Compiler/Source/ErrorRegistry.cpp
@@ -0,0 +1,9 @@
+#include "../Resources/ErrorRegistry.hpp";
+
+namespace Tarn::Errors
+{
+    std::string getHelpLinkFor(const std::string &code)
+    {
+        return "https://tarn-language.com/errors" + code;
+    }
+} // namespace Tarn::Errors
