053aa0a650862dde6dd0dc55d69059f1c0f3827c
Tarn Holder <contact@tarn-language.com>
2025-06-14 17:45:52 -0400

    Update `(/Compiler/Resources/Error*)`

---
+++ Compiler/Resources/ErrorLogic.hpp
@@ -0,0 +1,31 @@
+#pragma once
+
+#include "./ErrorTypes.hpp";
+#include "./ErrorRegistry.hpp";
+#include "./ErrorBuilder.hpp";
+#include "./ErrorEmitter.hpp";
+#include "./ErrorSpan.hpp";
+
+namespace Tarn::ErrorLogic
+{
+    extern ErrorEmitter *GlobalEmitter;
+
+    void reportTypeMismatch(
+        const Span &span,
+        const std::string &expectedType,
+        const std::string &foundType);
+
+    void reportUndeclaredVariable(
+        const Span &span,
+        const std::string &variableName);
+    void reportMissingSemicolon(
+        const Span &span);
+
+    void reportInternalCompilerError(
+        const Span &span,
+        const std::string &context);
+
+    void reportUnusedVariable(
+        const Span &span,
+        const std::string &name);
+} // namespace Tarn::ErrorLogic
